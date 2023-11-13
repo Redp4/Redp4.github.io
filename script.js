da2b0a572b0e75ce09166a2a9942b82a86811eaa
Epilez <paolo.pezzarossa2@gmail.com>
2023-11-13 17:14:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ fromTo('.sky', { y: 0 }, { y: -200 }, 0).
 fromTo('.cloud1', { y: 100 }, { y: -800 }, 0).
 fromTo('.cloud2', { y: -150 }, { y: -500 }, 0).
 fromTo('.cloud3', { y: -50 }, { y: -650 }, 0).
-
+fromTo('.mountBg', { y: -10 }, { y: -100 }, 0).
+fromTo('.mountMg', { y: -30 }, { y: -250 }, 0).
+fromTo('.mountFg', { y: -50 }, { y: -600 }, 0);
 
 $('#arrowBtn').on('mouseenter', e => {gsap.to('.arrow', { y: 10, duration: 0.8, ease: 'back.inOut(3)', overwrite: 'auto' });});
 $('#arrowBtn').on('mouseleave', e => {gsap.to('.arrow', { y: 0, duration: 0.5, ease: 'power3.out', overwrite: 'auto' });});
 $('#arrowBtn').on('click', e => {gsap.to(window, { scrollTo: innerHeight, duration: 1.5, ease: 'power1.inOut' });}); // scrollTo requires the ScrollTo plugin (not to be confused w/ ScrollTrigger)
+//# sourceURL=pen.js
